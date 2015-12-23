b3360f3e8e9e2b3f00c718a1a675c26de08769b7
Serge2702 <sergiogmv2702@gmail.com>
2015-12-22 21:45:52 -0600

    Ahora itera por directorios

--- Lineas_totales.sh
+++ Lineas_totales.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #Este archivo añade el número total de líneas a un archivo de texto
 
-for file in *
+for directory in  */
 do
-    lineas=$(wc -l $file | cut -f1 -d' ')
-    sed -i '1i ;' $file 
-    sed -i "1i $lineas" $file 
+    for file in "$directory"*
+    do
+        lineas=$(wc -l $file | cut -f1 -d' ')
+        sed -i '1i ;' $file 
+        sed -i "1i $lineas" $file 
+    done
 done
