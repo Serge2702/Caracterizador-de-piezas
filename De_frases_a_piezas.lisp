19ea7921e3ccf0c1ae7c9deeb9ec3077cca9808b
Serge2702 <sergiogmv2702@gmail.com>
2015-12-24 00:47:35 -0600

    Creada la función que convierte de frase a pieza

--- De_frases_a_piezas.lisp
+++ De_frases_a_piezas.lisp
@@ -6,13 +6,13 @@
 (defun procesa_armadura (arreglo)
   ;Procesa el archivo de 'Armadura_nuevo.txt'. En este caso sólo es necesario
   ;tomar el valor del primer objeto del arreglo.
-  (print (aref arreglo 0)))
+  (coerce (aref arreglo 0) 'list))
 
 (defun suma_meta_arreglos (meta_arreglo)
   ;Suma los arreglos uno a uno
   (let ((ceros (make-array (length (aref meta_arreglo 0)))))
     (loop for arreglo across meta_arreglo do
-          (setq ceros (map 'vector #'+ ceros arreglo)))
+          (setq ceros (map 'list #'+ ceros arreglo)))
     ceros))
 
 (defun columna_a_lista (arreglo indice)
@@ -34,14 +34,14 @@
 ;;6: Resta, 1 menos 2
 (defun procesa_ambito (arreglo)
   ;Procesa el archivo de Ámbito_Recortado
-  (let ((salida (make-array 7 :initial-element 0)))
-    (setf (aref salida 0) (apply #'min (columna_a_lista arreglo 0)))
-    (setf (aref salida 1) (apply #'max (columna_a_lista arreglo 1)))
-    (setf (aref salida 2) (apply #'min (columna_a_lista arreglo 2)))
-    (setf (aref salida 3) (apply #'max (columna_a_lista arreglo 3)))
-    (setf (aref salida 4) (- (aref salida 1) (aref salida 0)))
-    (setf (aref salida 5) (- (aref salida 3) (aref salida 2)))
-    (setf (aref salida 6) (- (aref salida 1) (aref salida 2)))
+  (let ((salida (make-list 7 :initial-element 0)))
+    (setf (nth 0 salida) (apply #'min (columna_a_lista arreglo 0)))
+    (setf (nth 1 salida) (apply #'max (columna_a_lista arreglo 1)))
+    (setf (nth 2 salida) (apply #'min (columna_a_lista arreglo 2)))
+    (setf (nth 3 salida) (apply #'max (columna_a_lista arreglo 3)))
+    (setf (nth 4 salida) (- (nth 1 salida) (nth 0 salida)))
+    (setf (nth 5 salida) (- (nth 3 salida) (nth 2 salida)))
+    (setf (nth 6 salida) (- (nth 1 salida) (nth 2 salida)))
     salida))
 
 (defun average (lista)
@@ -57,18 +57,38 @@
 (defun procesa_relaciones (arreglo)
   ;Procesa las relaciones. En los primeros 6 valores sólo es necesario calcular el
   ;promedio.
-  (let ((salida (make-array 7 :initial-element 0))
+  (let ((salida (make-list 7 :initial-element 0))
         (rh (apply #'+ (columna_a_lista arreglo 0)))
         (lh (apply #'+ (columna_a_lista arreglo 1))))
-    (setf (aref salida 0) (round-to (average (columna_a_lista arreglo 0)) 4))
-    (setf (aref salida 1) (round-to (average (columna_a_lista arreglo 1)) 4))
-    (setf (aref salida 2) (round-to (average (columna_a_lista arreglo 2)) 4))
-    (setf (aref salida 3) (round-to (average (columna_a_lista arreglo 3)) 4))
-    (setf (aref salida 4) (round-to (average (columna_a_lista arreglo 4)) 4))
-    (setf (aref salida 5) (round-to (average (columna_a_lista arreglo 5)) 4))
-    (setf (aref salida 6) 
+    (setf (nth 0 salida) (round-to (average (columna_a_lista arreglo 0)) 4))
+    (setf (nth 1 salida) (round-to (average (columna_a_lista arreglo 1)) 4))
+    (setf (nth 2 salida) (round-to (average (columna_a_lista arreglo 2)) 4))
+    (setf (nth 3 salida) (round-to (average (columna_a_lista arreglo 3)) 4))
+    (setf (nth 4 salida) (round-to (average (columna_a_lista arreglo 4)) 4))
+    (setf (nth 5 salida) (round-to (average (columna_a_lista arreglo 5)) 4))
+    (setf (nth 6 salida) 
           (cond
             ((= rh lh) 'B)
             ((> rh lh) 'R) 
             (t 'L) )) 
+    salida))
+
+;Ahora que tenemos las funciones para procesar los patrones, creamos la función
+;que une lo anterior para crear el nuevo patrón.
+(defun crea_patron_pieza nil
+  ;Crea el patrón por pieza. No recibe argumentos por que trabajará con todos los
+  ;archivos de la carpeta actual
+  (let ((armadura (read-file "Armadura_nuevo.txt"))
+        (tonos (read-file "Pitch_transpuesto.txt"))
+        (num_notas (read-file "Repeticiones_Num_Notas.txt"))
+        (relaciones (read-file "Relaciones_nuevo.txt"))
+        (octavas (read-file "Octavas_recortado.txt"))
+        (ambito (read-file "Ambito_recortado.txt"))
+        (salida))
+    (setq salida (append (procesa_armadura armadura) 
+                         (suma_meta_arreglos tonos) 
+                         (coerce (suma_cola (coerce (suma_meta_arreglos num_notas) 'vector) 4) 'list)
+                         (procesa_relaciones relaciones)
+                         (suma_meta_arreglos octavas)
+                         (procesa_ambito ambito)))
     salida))
