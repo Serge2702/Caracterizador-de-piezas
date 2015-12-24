2805cfc27d36dbdcbc59f4ace3064c87fc83ebf6
Serge2702 <sergiogmv2702@gmail.com>
2015-12-24 14:32:19 -0600

    Ya imprime los patrones a un archivo en la carpeta del compositor

--- Convertidor_automatico.sh
+++ Convertidor_automatico.sh
@@ -2,11 +2,15 @@
 # Este script convierte los patrones por frase a patrones por pieza usando el
 # otro código en common lisp. Pero esto lo imprime a un archivo para que sea por
 # artista.
-echo $1
+# Este Script debe ejecutarse directamente en la carpeta donde está contenido.
+# El argumento que recibe es la carpeta del artista. Esta debe ser relativa a la
+# posición de este script.
+
 for directorio in $1/*/
 do
-    echo "================================================================================"
     echo $directorio
     #sbcl --script "~/Documentos/Maestría/5to_Semestre/Tesis/Caracterizador de piezas/De_frases_a_piezas.lisp" "$directorio"
-    sbcl --script "De_frases_a_piezas.lisp" "$directorio"
+    sbcl --script "De_frases_a_piezas.lisp" "$directorio" >> "$1"/Patrones_piezas.txt
 done
+
+
