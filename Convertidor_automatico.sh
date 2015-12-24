917d4af00f8e47d5f10be31c4868fabaca8d9b9e
Serge2702 <sergiogmv2702@gmail.com>
2015-12-24 14:20:14 -0600

    Ya procesa todo por directorio

--- Convertidor_automatico.sh
+++ Convertidor_automatico.sh
@@ -2,8 +2,11 @@
 # Este script convierte los patrones por frase a patrones por pieza usando el
 # otro código en common lisp. Pero esto lo imprime a un archivo para que sea por
 # artista.
-
-for directorio in */
+echo $1
+for directorio in $1/*/
 do
-    sbcl --script "~/Documentos/Maestría/5to_Semestre/Tesis/Caracterizador de piezas/De_frases_a_piezas.lisp"
+    echo "================================================================================"
+    echo $directorio
+    #sbcl --script "~/Documentos/Maestría/5to_Semestre/Tesis/Caracterizador de piezas/De_frases_a_piezas.lisp" "$directorio"
+    sbcl --script "De_frases_a_piezas.lisp" "$directorio"
 done
