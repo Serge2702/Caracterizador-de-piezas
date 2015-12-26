e88f043b93a82b6178a1cdbb5112121c3b3e8fd6
Serge2702 <sergiogmv2702@gmail.com>
2015-12-25 23:41:34 -0600

    Creado para realizar automáticamente la clasificación con todas los archivos de propiedades

---
+++ Clasificador_automatico.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Este script realiza la clasificación automática de todas las piezas usando
+# todos los archivos de propiedades encontradas.
+
+for archivo in Resultados/*
+do
+    sbcl --script "Script_clasificador_universal.lisp" "$archivo" >> Clasificacion_"$(basename $archivo)"
+done
