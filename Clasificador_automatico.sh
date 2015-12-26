54fb670e5ed0f47efd8504c73863b640ca0498f2
Serge2702 <sergiogmv2702@gmail.com>
2015-12-25 23:43:04 -0600

    Crea los archivos en la carpeta de resultados

--- Clasificador_automatico.sh
+++ Clasificador_automatico.sh
@@ -4,5 +4,5 @@
 
 for archivo in Resultados/*
 do
-    sbcl --script "Script_clasificador_universal.lisp" "$archivo" >> Clasificacion_"$(basename $archivo)"
+    sbcl --script "Script_clasificador_universal.lisp" "$archivo" >> Resultados/Clasificacion_"$(basename $archivo)"
 done
