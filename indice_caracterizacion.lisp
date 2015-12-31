be57db04a6f62c9b2d39ea4fdb06ebae60725694
Serge2702 <sergiogmv2702@gmail.com>
2015-12-30 23:24:56 -0600

    Añadidas algunas funciones

--- indice_caracterizacion.lisp
+++ indice_caracterizacion.lisp
@@ -33,6 +33,49 @@
             (t nil)))
     salida))
 
+(defun obtener_todos_pares_+ (propiedades)
+  ;De un archivo de características obtiene los pares positivos de la clase
+  ;indicada.
+  (let ((conteos (contar_tipos_propiedades propiedades))
+        (salida)
+        (contador 0)
+        (tipo)
+        (cantidad)
+        (ordenados))
+    (setq cantidad (loop for k from 0 to 5 sum (aref conteos k 0)))
+    (setq salida (list (make-array cantidad)(make-array cantidad)))
+    (loop for propiedad across propiedades until (= cantidad contador) do
+          (setq tipo (nth 2 (nth 1 propiedad)))
+          (cond
+            ((equal (second tipo) '+)
+             (setq ordenados (sort (copy-list (coerce (nth 1 (nth 1 propiedad)) 'list)) #'>))
+             (setf (aref (nth 0 salida) contador) (nth 0 ordenados))
+             (setf (aref (nth 1 salida) contador) (nth 1 ordenados))
+             (incf contador))
+            (t nil)))
+    salida))
+
+(defun resta_todos_pares_+ (propiedades)
+  ;De un archivo de características obtiene la resta de todos los pares
+  ;positivos.
+  (let ((conteos (contar_tipos_propiedades propiedades))
+        (salida)
+        (contador 0)
+        (tipo)
+        (cantidad)
+        (ordenados))
+    (setq cantidad (loop for k from 0 to 5 sum (aref conteos k 0)))
+    (setq salida (make-array cantidad))
+    (loop for propiedad across propiedades until (= cantidad contador) do
+          (setq tipo (nth 2 (nth 1 propiedad)))
+          (cond
+            ((equal (second tipo) '+)
+             (setq ordenados (sort (copy-list (coerce (nth 1 (nth 1 propiedad)) 'list)) #'>))
+             (setf (aref salida contador) (- (nth 0 ordenados)(nth 1 ordenados)))
+             (incf contador))
+            (t nil)))
+    salida))
+
 (defun obtener_pares_- (conjunto nclase cantidad)
   ;De un archivo de características obtiene los pares negativos de la clase
   ;indicada.
@@ -52,7 +95,20 @@
             (t nil)))
     salida))
 
-(defun resta_ (argumentos)
-;Descripción
-  
+(defun promedio (lista)
+  ;Regresa el promedio de una lista
+  (/ (reduce #'+ lista) (length lista) 1.0))
+
+(defun indice_intento_1 (lista)
+;Primer intento para determinar el índice
+  (let ((salida))
+   (promedio (map 'list #'- (first lista) (second lista)))))
+
+(defun todos_los_positivos (propiedades)
+;Obtiene los pares de todos los números positivos
+  (let ((totales (contar_tipos_propiedades propiedades)))
+    (loop for k from 0 to 5 collect
+     (obtener_pares_+ propiedades k (aref totales k 0))
+     )
+    )
   )
