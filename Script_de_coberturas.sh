279123ed4b53d756036ff97c657c014c632ec19d
Serge2702 <sergiogmv2702@gmail.com>
2016-01-06 22:57:51 -0600

    Corrigiendo más errores

--- Script_de_coberturas.sh
+++ Script_de_coberturas.sh
@@ -3,10 +3,10 @@
 
 for archivo in Resultados/*.txt
 do
-    numeros=$(cat $archivo | grep -o '[0-9]\+-[0-9]\+')
+    numeros=$(echo $archivo | grep -o '[0-9]\+-[0-9]\+')
     alfa=$(echo $numeros | cut -d'-' -f1)
     beta=$(echo $numeros | cut -d'-' -f2)
     echo "$archivo: $alfa - $beta"
-    echo "$archivo" >> Resultados/Calculo_coverturas.txt
+    echo "$archivo" >> Resultados/Calculo_coberturas_corregido.txt
     sbcl --script Cobertura.lisp "$archivo" $alfa $beta >> Resultados/Calculo_coberturas_corregido.txt
 done
